586a4b8a413cee914ad44dcc5cb197c8da7eb0dd
n0fre <kasperblom@gmail.com>
2021-03-31 16:17:15 +0200

    Fixed bugs: 1) multithreading would cause files to be wrongly named 2) program would crash if it encountered a page without PDFs.

--- EMAscraper.py
+++ EMAscraper.py
@@ -3,7 +3,8 @@
 # Downloads all PDFs from European Medicines Agency search results to ./output
 
 # Define the URL of the EMA search action here
-url = 'https://www.ema.europa.eu/en/medicines/ema_group_types/ema_pip/field_ema_pip_decision_type%253Aname_field/RP%3A%20decision%20refers%20to%20a%20refusal%20on%20a%20proposed%20Paediatric%20Investigation%20Plan'
+url = 'https://www.ema.europa.eu/en/medicines/field_ema_web_categories%253Aname_field/Human'
+#url2 = 'https://www.ema.europa.eu/en/medicines/human/paediatric-investigation-plans/emea-002503-pip01-18'
 
 # Load the libraries
 import requests, re, sys, os, math, threading
@@ -18,6 +19,7 @@ links = []
 downloadThreads = []
 maxThreads = 100
 sema = threading.Semaphore(value=maxThreads)
+screenLock = threading.Semaphore(value=1)
 
 # Defines the function to request the page from a url and parse it
 def getPageAndParse(url):
@@ -29,14 +31,12 @@ def getPageAndParse(url):
         print('There was a problem: %s' % (exc))
     soup = BeautifulSoup(res.text, 'html.parser')
 
-
 # Gets the search hit links from the current page
 def getLinksFromPage(url):
     global links
     getPageAndParse(url)
     for link in soup.select('.ecl-list-item__link'):
         links.append(link.get('href'))
-
 
 # Gets the total number of search hits and calculates the total number of pages required to loop through
 getPageAndParse(url)
@@ -48,36 +48,41 @@ for i in range(0, numPages):
     urlPage = url + '?page=' + str(i)
     getLinksFromPage(urlPage)
 
-
 # Downloads all PDFs from the search hit
 def downloadPDFs(url):
     getPageAndParse(url)
-    headerElem = soup.select('h1')[0].text
-
-    print('This is page: ' + url + ' (%s)' % (headerElem))
-    if r"/" in headerElem:  # Replaces slashes with dashes in header element
-        headerElem = headerElem.replace("/", "-")
 
     links = soup.findAll('a', href=re.compile("\.pdf$"))
 
     pdfNumber = 0
 
-    for el in links:
-        pdfNumber = pdfNumber + 1
-        print("Downloading pdf: " + el['href'])
-        pdfURL = el['href']
+    if links != []:
+        for el in links:
+            pdfNumber = pdfNumber + 1
+            headerElem = soup.select('h1')[0].text
 
-        res = requests.get(pdfURL)
-        try:
-            res.raise_for_status()
-        except Exception as exc:
-            print('There was a problem: %s' % (exc))
+            screenLock.acquire()
+            print('This is page: ' + url + ' (%s)' % (headerElem))
+            if r"/" in headerElem:  # Replaces slashes with dashes in header element
+                headerElem = headerElem.replace("/", "-")
 
-        pdfFile = open(Path(directory, 'output', headerElem + '_' + str(pdfNumber).zfill(2) + '.pdf'), 'wb')
-        for chunk in res.iter_content(100000):
-            pdfFile.write(chunk)
+            print("Downloading pdf: " + el['href'])
+            screenLock.release()
 
-    pdfFile.close()
+            pdfURL = el['href']
+
+            res = requests.get(pdfURL)
+            try:
+                res.raise_for_status()
+            except Exception as exc:
+                print('There was a problem: %s' % (exc))
+
+            pdfFile = open(Path(directory, 'output', headerElem + '_' + str(pdfNumber).zfill(2) + '.pdf'), 'wb')
+            for chunk in res.iter_content(100000):
+                pdfFile.write(chunk)
+        pdfFile.close()
+    else:
+        print('No PDFs found.')
 
 # Downloads the PDFs from the list of search hits
 def processSearchHits(SearchHit):
